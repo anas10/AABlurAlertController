abae488cc7b5eed6cc1a4361ee3aef5c6fdfc3c5
Anas AIT ALI <aitali.anas@gmail.com>
2017-01-20 12:39:44 +0000

    Added tests

--- Example/AABlurAlertControllerTests/AABlurAlertControllerTests.swift
+++ Example/AABlurAlertControllerTests/AABlurAlertControllerTests.swift
@@ -6,31 +6,87 @@
 //
 //
 
-import XCTest
+import Quick
+import Nimble
+import Nimble_Snapshots
+
 @testable import AABlurAlertController
 
-class AABlurAlertControllerTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class AABlurAlertControllerTests: QuickSpec {
+
+    override func spec() {
+        var aaBlurAlertController: AABlurAlertController!
+
+        describe("a AABlurAlertController") {
+            beforeEach {
+                let window = UIWindow(frame: UIScreen.main.bounds)
+                window.rootViewController = UIViewController()
+                window.makeKeyAndVisible()
+                aaBlurAlertController = AABlurAlertController()
+            }
+
+            it("looks good with image, title, subtitle and 2 buttons") {
+                let subject = self.testSubject(aaBlurAlertController)
+                expect(subject).to(haveValidDeviceAgnosticSnapshot())
+            }
+
+            it("looks good with image, title, subtitle and 1 button") {
+                let subject = self.testSubject(aaBlurAlertController, image: true, title: true, subtitle: true, numberOfBtn: 1)
+                expect(subject).to(haveValidDeviceAgnosticSnapshot())
+            }
+
+            it("looks good with image, title and 2 buttons") {
+                let subject = self.testSubject(aaBlurAlertController, image: true, title: true, subtitle: false)
+                expect(subject).to(haveValidDeviceAgnosticSnapshot())
+            }
+
+            it("looks good with title, subtitle and 2 buttons") {
+                let subject = self.testSubject(aaBlurAlertController, image: false)
+                expect(subject).to(haveValidDeviceAgnosticSnapshot())
+            }
+
+            it("looks good with title and 2 buttons") {
+                let subject = self.testSubject(aaBlurAlertController, image: false, title: true, subtitle: false)
+                expect(subject).to(haveValidDeviceAgnosticSnapshot())
+            }
+
+            it("looks good with title, long subtitle and 2 buttons") {
+                let subject = self.testSubject(aaBlurAlertController, image: false)
+                subject.alertSubtitle.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+                expect(subject).to(haveValidDeviceAgnosticSnapshot())
+            }
+
+            it("looks good with long title, subtitle and 2 buttons") {
+                let subject = self.testSubject(aaBlurAlertController, image: false)
+                subject.alertTitle.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+                expect(subject).to(haveValidDeviceAgnosticSnapshot())
+            }
+
+            it("looks good with title, subtitle and no buttons") {
+                let subject = self.testSubject(aaBlurAlertController, image: false, title: true, subtitle: true, numberOfBtn: 0)
+                expect(subject).to(haveValidDeviceAgnosticSnapshot())
+            }
         }
     }
-    
+
+}
+
+extension AABlurAlertControllerTests {
+    func testSubject(_ blurAlertController: AABlurAlertController,
+                     image: Bool = true,
+                     title: Bool = true,
+                     subtitle: Bool = true,
+                     numberOfBtn: Int = 2
+        ) -> AABlurAlertController {
+        if image {blurAlertController.alertImage.image = UIImage(named: "illustration_profile_avatar")}
+        if title {blurAlertController.alertTitle.text = "Start a job ?"}
+        if subtitle {blurAlertController.alertSubtitle.text = "Are you sure you would like to start the job?\nThis action cannot be undone."}
+        if numberOfBtn >= 1 {
+            blurAlertController.addAction(action: AABlurAlertAction(title: "Cancel", style: AABlurActionStyle.cancel, handler: nil))
+        }
+        if numberOfBtn >= 2 {
+            blurAlertController.addAction(action: AABlurAlertAction(title: "Start", style: AABlurActionStyle.default, handler: nil))
+        }
+        return blurAlertController
+    }
 }
